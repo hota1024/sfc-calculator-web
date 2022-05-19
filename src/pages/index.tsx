95af1ae9cd0105fee74a9bc5c41d13a8230a8f02
hota1024 <hotalog@hota1024.com>
2022-05-19 09:53:49 +0900

    🎉init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - calculator-web-app</title>
+      </Head>
+
+      <div>calculator-web-app</div>
+    </>
+  )
+}
+
+export default HomePage
